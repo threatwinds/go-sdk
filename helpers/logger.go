d9cb48a89dfafaf37915d3956623e23f628f6d54
Osmany Montero <osmontero@icloud.com>
2024-06-13 17:12:20 +0300

    limiting line size to divided editor compatibility

--- helpers/logger.go
+++ helpers/logger.go
@@ -12,13 +12,20 @@ var loggerOnce sync.Once
 func NewLogger(level int) *logger.Logger {
 	loggerOnce.Do(func() {
 		loggerInstance = logger.NewLogger(&logger.Config{
-			Level:     level,
-			Format:    "text",
-			Retries:   3,
-			Wait:      5,
+			Level:   level,
+			Format:  "text",
+			Retries: 3,
+			Wait:    5,
 			StatusMap: map[int][]string{
-				400: {"missing", "invalid"},
-				100: {"no such file or directory", "signal: interrupt", "context canceled"},
+				400: {
+					"missing",
+					"invalid",
+				},
+				100: {
+					"no such file or directory",
+					"signal: interrupt",
+					"context canceled",
+				},
 			},
 		})
 	})
--- helpers/request.go
+++ helpers/request.go
@@ -9,12 +9,17 @@ import (
 	"github.com/threatwinds/logger"
 )
 
-func DoReq[response any](url string, data []byte, method string, headers map[string]string) (response, int, *logger.Error) {
+func DoReq[response any](url string,
+	data []byte, method string,
+	headers map[string]string) (response, int, *logger.Error) {
+
 	var result response
 
 	req, err := http.NewRequest(method, url, bytes.NewBuffer(data))
 	if err != nil {
-		return result, http.StatusInternalServerError, Logger().ErrorF("error creating request: %s", err.Error())
+		return result,
+			http.StatusInternalServerError,
+			Logger().ErrorF("error creating request: %s", err.Error())
 	}
 
 	for k, v := range headers {
@@ -25,23 +30,34 @@ func DoReq[response any](url string, data []byte, method string, headers map[str
 
 	resp, err := client.Do(req)
 	if err != nil {
-		return result, http.StatusInternalServerError, Logger().ErrorF("error sending request: %s", err.Error())
+		return result,
+			http.StatusInternalServerError,
+			Logger().ErrorF("error sending request: %s", err.Error())
 	}
 
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return result, http.StatusInternalServerError, Logger().ErrorF("error reading response: %s", err.Error())
+		return result,
+			http.StatusInternalServerError,
+			Logger().ErrorF("error reading response: %s", err.Error())
 	}
 
-	if resp.StatusCode != http.StatusAccepted && resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusNoContent {
-		return result, resp.StatusCode, Logger().ErrorF("received status code %d", resp.StatusCode)
+	if resp.StatusCode != http.StatusAccepted &&
+		resp.StatusCode != http.StatusOK &&
+		resp.StatusCode != http.StatusNoContent {
+
+		return result,
+			resp.StatusCode,
+			Logger().ErrorF("received status code %d", resp.StatusCode)
 	}
 
 	err = json.Unmarshal(body, &result)
 	if err != nil {
-		return result, http.StatusInternalServerError, Logger().ErrorF("error unmarshalling response: %s", err.Error())
+		return result,
+			http.StatusInternalServerError,
+			Logger().ErrorF("error unmarshalling response: %s", err.Error())
 	}
 
 	return result, resp.StatusCode, nil
