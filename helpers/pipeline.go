f105c2a2a3aa003681ea9dcbfb12c479cac92266
Osmany Montero <osmontero@icloud.com>
2024-06-27 19:08:37 +0300

    removing some unused fields

--- helpers/pipeline.go
+++ helpers/pipeline.go
@@ -54,7 +54,6 @@ type Kv struct {
 type Property struct {
 	FieldSplit string `yaml:"field_split"`
 	ValueSplit string `yaml:"value_split"`
-	TrimBefore []Trim `yaml:"trim_before"`
 	TrimValue  []Trim `yaml:"trim_value"`
 }
 
