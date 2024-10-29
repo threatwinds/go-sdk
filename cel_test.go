292777937da4acfc208dffa7b02bb5504516a06e
Osmany Montero <osmontero@icloud.com>
2024-10-29 06:10:22 -0400

    more testing

--- cel_test.go
+++ cel_test.go
@@ -3,25 +3,51 @@ package go_sdk
 import "testing"
 
 func TestEvaluate(t *testing.T) {
-	w := Where{
-		Variables: []*Variable{
-			{
-				Get: "field1",
-				As: "field1",
-				OfType: "string",
+	t.Run("Equals", func(t *testing.T) {
+		w := Where{
+			Variables: []*Variable{
+				{
+					Get: "field1",
+					As: "field1",
+					OfType: "string",
+				},
+				{
+					Get: "field2",
+					As: "field2",
+					OfType: "string",
+				},
 			},
-			{
-				Get: "field2",
-				As: "field2",
-				OfType: "string",
+			Expression: "field1 == field2",
+		}
+	
+		equal := w.Evaluate(PointerOf(`{"field1": "value1", "field2": "value1"}`))
+	
+		if !equal {
+			t.Errorf("Expected true, got false")
+		}
+	})
+
+	t.Run("Not Equals", func(t *testing.T) {
+		w := Where{
+			Variables: []*Variable{
+				{
+					Get: "field1",
+					As: "field1",
+					OfType: "string",
+				},
+				{
+					Get: "field2",
+					As: "field2",
+					OfType: "string",
+				},
 			},
-		},
-		Expression: "field1 == field2",
-	}
-
-	equal := w.Evaluate(PointerOf(`{"field1": "value1", "field2": "value1"}`))
-
-	if !equal {
-		t.Errorf("Expected true, got false")
-	}
+			Expression: "field1 == field2",
+		}
+	
+		equal := w.Evaluate(PointerOf(`{"field1": "value0", "field2": "value1"}`))
+	
+		if equal {
+			t.Errorf("Expected true, got false")
+		}
+	})
 }
