063c41090352539093575606230455216529f55d
Osmany Montero <osmontero@icloud.com>
2024-10-29 06:37:17 -0400

    test startsWith

--- cel_test.go
+++ cel_test.go
@@ -47,6 +47,25 @@ func TestEvaluate(t *testing.T) {
 		equal := w.Evaluate(PointerOf(`{"field1": "value0", "field2": "value1"}`))
 	
 		if equal {
+			t.Errorf("Expected false, got true")
+		}
+	})
+
+	t.Run("StartsWith", func(t *testing.T) {
+		w := Where{
+			Variables: []*Variable{
+				{
+					Get: "log.field",
+					As: "field",
+					OfType: "string",
+				},
+			},
+			Expression: "field.startsWith('8.1.0')",
+		}
+	
+		startsWith := w.Evaluate(PointerOf(`{"log": {"field": "8.1.0-20241029"}}`))
+	
+		if !startsWith {
 			t.Errorf("Expected true, got false")
 		}
 	})
