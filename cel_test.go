63abd6e3bb3d91a61caee71d89f821fd8d3a817d
Osmany Montero <osmontero@icloud.com>
2024-10-29 06:08:28 -0400

    test for CEL

---
+++ cel_test.go
@@ -0,0 +1,27 @@
+package go_sdk
+
+import "testing"
+
+func TestEvaluate(t *testing.T) {
+	w := Where{
+		Variables: []*Variable{
+			{
+				Get: "field1",
+				As: "field1",
+				OfType: "string",
+			},
+			{
+				Get: "field2",
+				As: "field2",
+				OfType: "string",
+			},
+		},
+		Expression: "field1 == field2",
+	}
+
+	equal := w.Evaluate(PointerOf(`{"field1": "value1", "field2": "value1"}`))
+
+	if !equal {
+		t.Errorf("Expected true, got false")
+	}
+}
