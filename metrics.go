99b788de935ced94633b4e1de22eb94c3af4b862
Osmany Montero <osmontero@icloud.com>
2024-12-04 07:21:42 -0500

    adding documentation

--- metrics.go
+++ metrics.go
@@ -2,17 +2,35 @@ package go_sdk
 
 import "time"
 
+// Metter represents a structure to measure the execution time of a function.
+// It contains the start time, the name of the function, and additional options.
 type Metter struct {
 	StartTime time.Time
 	Function  string
 	Options   MetterOptions
 }
 
+// MetterOptions defines the configuration options for metering.
+// It includes options to log slow operations and set a threshold for what is considered slow.
+//
+// Fields:
+//   LogSlow: A boolean indicating whether to log operations that are considered slow.
+//   SlowThreshold: A time.Duration value that specifies the threshold duration for slow operations.
 type MetterOptions struct {
 	LogSlow bool
 	SlowThreshold time.Duration
 }
 
+// NewMetter creates a new Metter instance to measure the execution time of a function.
+// It takes a function name as a string and an optional list of MetterOptions.
+// If no options are provided, it defaults to logging slow executions with a threshold of 500 milliseconds.
+//
+// Parameters:
+//   - function: The name of the function to be measured.
+//   - options: Optional MetterOptions to customize the behavior of the Metter.
+//
+// Returns:
+//   A pointer to a Metter instance.
 func NewMetter(function string, options ...MetterOptions) *Metter {
 	if len(options) == 0 {
 		options = []MetterOptions{{LogSlow: true, SlowThreshold: 500 * time.Millisecond}}
@@ -25,6 +43,10 @@ func NewMetter(function string, options ...MetterOptions) *Metter {
 	}
 }
 
+// Elapsed calculates the duration since the Metter's StartTime.
+// If the LogSlow option is enabled and the elapsed time exceeds 500 milliseconds,
+// it logs an informational message indicating that the function is slow.
+// Returns the elapsed time as a time.Duration.
 func (m *Metter) Elapsed() time.Duration {
 	elapsed := time.Since(m.StartTime)
 	if m.Options.LogSlow {
