9d790bd00faaade1f74b8c1033640b9d601e86a1
Osmany Montero <osmontero@icloud.com>
2024-12-12 08:46:33 -0500

    allow conditional empty fields

--- fields.go
+++ fields.go
@@ -3,9 +3,13 @@ package go_sdk
 import "errors"
 
 // ValidateReservedField validates a field to ensure it is not empty or a reserved field.
-func ValidateReservedField(f string) error {
+func ValidateReservedField(f string, allowEmpty bool) error {
 	switch f {
 	case "":
+		if allowEmpty {
+			return nil
+		}
+		
 		return errors.New("field cannot be empty")
 	case "raw":
 		return errors.New("field cannot be 'raw'")
