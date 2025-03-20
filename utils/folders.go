a3141d0a19ef6c6d7ba9d9652ddcfd8be9926980
Osmany Montero <osmontero@icloud.com>
2025-03-20 16:38:47 +0000

    Refactor folder handling with Folder type and methods
    Introduce the Folder type to encapsulate folder operations, improving clarity and type safety. Refactor FileJoin into a method of Folder and add a String method for folder string representation. These changes enhance maintainability and promote cleaner code.

--- utils/folders.go
+++ utils/folders.go
@@ -5,6 +5,8 @@ import (
 	"path/filepath"
 )
 
+type Folder string
+
 // MkdirJoin creates a directory structure specified by the joined path of given parts and returns the complete path.
 // It uses `os.MkdirAll` with a default permission of 0755 for creating directories.
 // Returns the resulting path and an error if any occurs during directory creation.
@@ -14,7 +16,12 @@ func MkdirJoin(f ...string) (string, error) {
 	return address, err
 }
 
-// FileJoin concatenates multiple path components into a single path using platform-specific path separators.
-func FileJoin(f ...string) string {
-	return filepath.Join(f...)
+// FileJoin concatenates folder and file components into a single path using platform-specific path separators.
+func (folder *Folder) FileJoin(file string) string {
+	return filepath.Join(folder.String(), file)
+}
+
+// String returns the string representation of the Folder type.
+func (folder *Folder) String() string {
+	return string(*folder)
 }
