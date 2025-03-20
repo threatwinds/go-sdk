cc5be5c853266bba846407adacb9a2b5b922940c
Osmany Montero <osmontero@icloud.com>
2025-03-20 16:34:17 +0000

    Switch from `path` to `filepath` for path handling
    Replaced `path.Join` with `filepath.Join` to ensure proper handling of paths on all platforms, as `filepath` respects OS-specific path separators. Updated `MkdirJoin` and `FileJoin` functions accordingly for improved cross-platform compatibility.

--- utils/folders.go
+++ utils/folders.go
@@ -2,15 +2,19 @@ package utils
 
 import (
 	"os"
-	"path"
+	"path/filepath"
 )
 
+// MkdirJoin creates a directory structure specified by the joined path of given parts and returns the complete path.
+// It uses `os.MkdirAll` with a default permission of 0755 for creating directories.
+// Returns the resulting path and an error if any occurs during directory creation.
 func MkdirJoin(f ...string) (string, error) {
-	address := path.Join(f...)
+	address := filepath.Join(f...)
 	err := os.MkdirAll(address, 0755)
 	return address, err
 }
 
+// FileJoin concatenates multiple path components into a single path using platform-specific path separators.
 func FileJoin(f ...string) string {
-	return path.Join(f...)
+	return filepath.Join(f...)
 }
